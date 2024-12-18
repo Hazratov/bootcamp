2166131f7ce302c04dd37dcae455b66411404611
Hazratov Behro'z <hazratqulovbehruz60@gmail.com>
2024-12-18 18:05:29 +0500

    change cascade

--- django/first/spotify/music/models/song.py
+++ django/first/spotify/music/models/song.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Song(models.Model):
-    album = models.ForeignKey('music.Album')
+    album = models.ForeignKey('music.Album', on_delete=models.CASCADE)
     title = models.CharField(max_length=150, blank=False, null=False)
     cover = models.URLField(blank=True)
     source = models.URLField(blank=False, null=False)
